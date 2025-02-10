22fdc6c24fb3412d78c3d81225c23ffb59f58d9f
Aadarsh Raj <aadarshraj351@gmail.com>
2025-02-10 11:59:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,20 @@
-// complete this js code
-function Person(name, age) {}
+// Constructor function for Person
+function Person(name, age) {
+  this.name = name;
+  this.age = age;
+}
 
-function Employee(name, age, jobTitle) {}
+// Constructor function for Employee (inherits from Person)
+function Employee(name, age, jobTitle) {
+  Person.call(this, name, age); // Call the Person constructor
+  this.jobTitle = jobTitle;
+}
+
+// Ensure prototype inheritance
+Employee.prototype = Object.create(Person.prototype);
+Employee.prototype.constructor = Employee;
 
 // Do not change code below this line
 window.Person = Person;
 window.Employee = Employee;
+
