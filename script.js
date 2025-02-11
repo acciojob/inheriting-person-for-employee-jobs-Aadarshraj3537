c7058ca11c39fe7fc926ef619b840f0fea5e2196
Aadarsh Raj <aadarshraj351@gmail.com>
2025-02-11 12:35:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ class Person {
 
 class Employee extends Person {
   constructor(name, age, jobTitle) {
-    super(name, age); 
+    super(name, age);
     this.jobTitle = jobTitle;
   }
 
@@ -28,32 +28,3 @@ person.greet();
 const employee = new Employee("Bob", 30, "Manager");
 employee.jobGreet();
 
-class Person {
-  constructor(name, age) {
-    this.name = name;
-    this.age = age;
-  }
-
-  greet() {
-    console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`);
-  }
-}
-
-class Employee extends Person {
-  constructor(name, age, jobTitle) {
-    super(name, age);
-    this.jobTitle = jobTitle;
-  }
-
-  jobGreet() {
-    console.log(
-      `Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`
-    );
-  }
-}
-
-const person = new Person("Alice", 25);
-person.greet(); 
-
-const employee = new Employee("Bob", 30, "Manager");
-employee.jobGreet(); 
